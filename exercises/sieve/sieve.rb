c224731cb38cc2ca9d2de3f2b3b25c22eb980262
Wesley Gomes <gomes.wesley@gmail.com>
2018-02-17 10:46:32 +0100

    Sieve V2, benchmarks and wiki implementation code

--- exercises/sieve/sieve.rb
+++ exercises/sieve/sieve.rb
@@ -1,3 +1,4 @@
+require "benchmark"
 class Sieve
   LOWER_LIMIT=2
   def initialize(limit)
@@ -6,36 +7,83 @@ class Sieve
     @primes = []
   end
 
+  def primes_with_benchmark
+    numbers = 0
+    benchmark = Benchmark.measure do
+      numbers = primes
+    end
+    puts "Time: #{benchmark.total} sec"
+    [benchmark, numbers.size]
+  end
+
   def primes
     return [] if @limit < LOWER_LIMIT
-    for n in 2..@limit do
-      prime = get_next_unmarked n
-      mark_all_multiples prime
-      add_prime prime if prime <= @limit
+    current = LOWER_LIMIT
+    while true
+      current = get_next_unmarked current
+      mark_all_multiples current
+      add_prime current
+      current += 1
+      break if current >= @limit
     end
     @primes
   end
 
-  def add_prime prime
-    @primes << prime unless @primes.include? prime
-  end
-
-  def get_next_unmarked current
-    return current unless @primes.any?
-    unless (@marked_list.include? current)
-      return current
-    else
-      current += 1
-      get_next_unmarked current
+  def wiki_benchmark
+    primes = []
+    measure = Benchmark.measure do
+      primes = wiki_implementation
     end
+    [primes.size, measure]
+  end
+  def wiki_implementation
+    a = []
+    0.upto(@limit) do |number|
+      a << true
+    end
+    primes = []
+    for i in 2..@limit do
+      if a[i]
+        j=i**2
+        counter = 1
+        while true do
+          a[j] = false
+          j=(i**2)+counter*i
+          break if j > @limit
+          counter += 1
+        end
+        primes << i
+      end
+    end
+    primes
   end
 
-  def mark_all_multiples prime_number
-    return "" unless prime_number
-    lower_limit = prime_number + 1
-    @marked_list << (lower_limit..@limit).to_a.select{ |n| n % prime_number == 0 }
-    @marked_list.flatten!
-  end
+  private
+    def add_prime prime
+      @primes << prime unless prime > @limit
+    end
+
+    def get_next_unmarked current
+      return current unless @primes.any?
+      unless (@marked_list.include? current)
+        return current
+      else
+        current += 1
+        get_next_unmarked current
+      end
+    end
+
+    def mark_all_multiples prime_number
+      lower_limit = prime_number + 1
+      not_marked = (lower_limit..@limit).to_a - (@marked_list)
+      not_marked.select! {|n| is_multiple? prime_number, n}
+      @marked_list = @marked_list + not_marked
+      @marked_list
+    end
+
+    def is_multiple? prime, candidate
+      candidate % prime == 0
+    end
 end
 
 module BookKeeping
